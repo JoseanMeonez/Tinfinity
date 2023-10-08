ced15d347338cb780a8dba9e4f4336518c4080ab
Josean <joseanmeonez@gmail.com>
2023-10-07 22:47:34 -0600

    feat: chakras dto added to character info

---
+++ Tinfinity.Application/Dtos/ChakrasDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tinfinity.Application.Dtos
+{
+	public class ChakrasDto
+	{
+		public int Muscle { get; set; }
+		public int Nerve { get; set; }
+		public int Heart { get; set; }
+		public int Mental { get; set; }
+	}
+}
